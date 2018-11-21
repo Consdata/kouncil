6489fe9bd742d90ea780eee0066c73f59200dfdc
Jacek Grobelny <jgrobelny@consdata.pl>
2018-11-21 16:10:33 +0100

    obsluga powrotu po wyslaniu wiadomosci

--- kafka-companion-frontend/src/app/send/send.component.ts
+++ kafka-companion-frontend/src/app/send/send.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { Location } from '@angular/common';
 import { HttpClient } from "@angular/common/http";
 import { Message } from "app/topic/message";
 
@@ -11,21 +12,24 @@ import { Message } from "app/topic/message";
 export class SendComponent implements OnInit {
 
   topicName: string;
+  returnUrl: string;
   message: Message = new Message("", "", null, null, null);
   count: number = 1;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private http: HttpClient, private _location: Location) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.topicName = params['topic'];
+      this.returnUrl = params.returnUrl;
     });
   }
 
   onSubmit() {
     this.http.post(`/api/topic/send/${this.topicName}/${this.message.key}/${this.count}`, this.message.value).subscribe(data => {
       console.log("done");
+      this._location.back();
     });
   }
 
