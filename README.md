82c15de7f62677536923dd3013d9e24e61180fb6
Jacek Grobelny <jgrobelny@consdata.pl>
2018-11-21 13:07:13 +0100

    migracja do nowego adminClient w konsumentach

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Kafka Companion
 Simple frontend for browsing and adding messages to the Kafka topics.
+
+# Developent
+For backend, run KafkaCompanionApplication passing parameter ```bootstrapServers=localhost:9092```.
+
+For frontent, having ng-cli installed, run ```ng start``` preceeded by ```npm install```
+
+# Deployment
+```
+mvn package -P dist
+docker run -d -p 64000:8080 -e bootstrapServers="localhost:9092" --name kafka-companion tomlewlit/kafka-companion
+```
--- kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/KafkaConfiguration.java
+++ kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/KafkaConfiguration.java
@@ -1,8 +1,5 @@
 package pl.tomlewlit.kafkacompanion;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,33 +9,35 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class KafkaConfiguration {
 
-	private KafkaCompanionConfiguration kafkaCompanionConfiguration;
+    private KafkaCompanionConfiguration kafkaCompanionConfiguration;
 
-	@Autowired
-	public KafkaConfiguration(KafkaCompanionConfiguration kafkaCompanionConfiguration) {
-		this.kafkaCompanionConfiguration = kafkaCompanionConfiguration;
-	}
+    @Autowired
+    public KafkaConfiguration(KafkaCompanionConfiguration kafkaCompanionConfiguration) {
+        this.kafkaCompanionConfiguration = kafkaCompanionConfiguration;
+    }
 
-	@Bean
-	public ProducerFactory<String, String> producerFactory() {
-		return new DefaultKafkaProducerFactory<>(producerConfigs());
-	}
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
 
-	@Bean
-	public Map<String, Object> producerConfigs() {
-		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaCompanionConfiguration.getBootstrapServers());
-//		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return props;
-	}
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaCompanionConfiguration.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
 
-	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate() {
-		return new KafkaTemplate<String, String>(producerFactory());
-	}
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
