752d273c6c16622bd7f2dc60a732ac240d4e981e
Jacek Grobelny <jgrobelny@consdata.pl>
2019-01-29 16:25:00 +0100

    implementation

--- kafka-companion-frontend/src/app/brokers/brokers.component.ts
+++ kafka-companion-frontend/src/app/brokers/brokers.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { SearchService } from "app/search.service";
 import { Brokers } from "app/brokers/brokers";
-import { Broker } from "app/brokers/broker";
+import {Broker, BrokerConfig} from "app/brokers/broker";
 import { Subscription } from "rxjs/Subscription";
 
 @Component({
@@ -14,9 +14,10 @@ export class BrokersComponent implements OnInit {
 
   constructor(private http: HttpClient, private searchService: SearchService) {
   }
-
+  @ViewChild('table') table: any;
   allBrokers: Broker[];
   filteredBrokers: Broker[];
+  brokerConfigs: BrokerConfig[];
   private subscription: Subscription;
   phrase: string;
 
@@ -38,6 +39,28 @@ export class BrokersComponent implements OnInit {
     this.filteredBrokers = this.allBrokers.filter((broker) => {
       return !this.phrase || JSON.stringify(broker).toLowerCase().indexOf(this.phrase.toLowerCase()) > -1;
     });
+  }
+
+  toggleExpandRow(row) {
+    console.log('Toggled Expand Row!', row.id);
+    this.http.get("/api/configs/" + row.id)
+      .subscribe(data => {
+        console.log('Detail Toggled', data);
+        row.config = <BrokerConfig[]> data;
+        this.table.rowDetail.toggleExpandRow(row);
+      });
+
+  }
+
+  getRowHeight(row) {
+    console.log('getRowHeight', row);
+    if(!row) return 50;
+    if(row.config === undefined) return 50;
+    return row.config.size * 10;
+  }
+
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
   }
 
 }
