b89d9e7fc0165e1c0c62f293046f379d1ac11a40
Jacek Grobelny <jgrobelny@consdata.pl>
2019-01-29 16:25:29 +0100

    backend

---
+++ kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/BrokerConfig.java
@@ -0,0 +1,25 @@
+package pl.tomlewlit.kafkacompanion;
+
+import lombok.Builder;
+import lombok.Data;
+import org.apache.kafka.clients.admin.ConfigEntry;
+
+import java.util.Comparator;
+
+@Data
+@Builder
+public class BrokerConfig implements Comparable<BrokerConfig> {
+    private final String name;
+    private final String value;
+    private final ConfigEntry.ConfigSource source;
+    private final boolean isSensitive;
+    private final boolean isReadOnly;
+
+    @Override
+    public int compareTo(BrokerConfig o) {
+        return Comparator
+                .comparing(BrokerConfig::getName)
+                .thenComparing(BrokerConfig::getSource)
+                .compare(this, o);
+    }
+}
--- kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/BrokersController.java
+++ kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/BrokersController.java
@@ -1,16 +1,24 @@
 package pl.tomlewlit.kafkacompanion;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.Config;
+import org.apache.kafka.clients.admin.ConfigEntry;
 import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.Node;
+import org.apache.kafka.common.config.ConfigResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class BrokersController {
@@ -36,6 +44,28 @@ public class BrokersController {
                     .build()));
             Collections.sort(brokers);
             return Brokers.builder().brokers(brokers).build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/api/configs/{name}")
+    public Collection<BrokerConfig> getConfigs(@PathVariable("name") String name) {
+        try {
+            ConfigResource o = new ConfigResource(ConfigResource.Type.BROKER, name);
+            Collection<ConfigResource> resources = Collections.singletonList(o);
+            DescribeConfigsResult describeClusterResult = adminClient.describeConfigs(resources);
+            KafkaFuture<Config> nodes = describeClusterResult.values().get(o);
+            Collection<ConfigEntry> entries = nodes.get().entries();
+            List<BrokerConfig> configs = new ArrayList<>();
+            entries.forEach(e -> configs.add(BrokerConfig.builder()
+                    .name(e.name())
+                    .source(e.source())
+                    .value(e.value())
+                    .isReadOnly(e.isReadOnly())
+                    .isSensitive(e.isSensitive())
+                    .build()));
+            return configs.stream().sorted().collect(Collectors.toList());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
