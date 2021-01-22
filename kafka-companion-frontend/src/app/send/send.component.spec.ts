98477037e2f89b2e9200f70c0875a152c21a5ff3
Jacek Grobelny <jgrobelny@consdata.pl>
2021-01-22 10:34:47 +0100

    Angular update

--- kafka-companion-frontend/src/app/send/send.component.spec.ts
+++ kafka-companion-frontend/src/app/send/send.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SendComponent } from './send.component';
 
@@ -6,7 +6,7 @@ describe('SendComponent', () => {
   let component: SendComponent;
   let fixture: ComponentFixture<SendComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SendComponent ]
     })
