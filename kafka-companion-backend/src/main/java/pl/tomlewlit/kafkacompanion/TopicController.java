feeb65d482dc2ef8b3d1bd1b95cffd5a1132bcb9
Jacek Grobelny <jgrobelny@consdata.pl>
2021-02-11 18:16:04 +0100

    Backend for single partition

--- kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/TopicController.java
+++ kafka-companion-backend/src/main/java/pl/tomlewlit/kafkacompanion/TopicController.java
@@ -30,82 +30,92 @@ public class TopicController {
         this.kafkaCompanionConfiguration = kafkaCompanionConfiguration;
     }
 
-	@GetMapping("/api/topic/messages/{topicName}/{partition}/{offset}")
-	public TopicMessages getTopicMessages(@PathVariable("topicName") String topicName,
-										  @PathVariable("partition") String partitions,
-										  @PathVariable("offset") String offset) {
-		log.debug("TCM01 topicName={}, partition={}, offset={}", topicName, partitions, offset);
-		Properties props = createCommonProperties();
-		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
-			List<PartitionInfo> partitionInfos = consumer.partitionsFor(topicName);
-			log.debug("TCM02 partitionInfos.size={}, partitionInfos={}", partitionInfos.size(), partitionInfos);
-			List<TopicPartition> topicPartitions = new ArrayList<>();
-			for (int i = 0; i < partitionInfos.size(); i++) {
-				topicPartitions.add(new TopicPartition(topicName, i));
-			}
-			consumer.assign(topicPartitions);
+    @GetMapping("/api/topic/messages/{topicName}/{partition}/{offset}")
+    public TopicMessages getTopicMessages(@PathVariable("topicName") String topicName,
+                                          @PathVariable("partition") String partitions,
+                                          @Deprecated @PathVariable("offset") String offset,
+                                          @RequestParam("sort") String sort,
+                                          @RequestParam("order") String order,
+                                          @RequestParam("offset") String offsetShiftParam,
+                                          @RequestParam("limit") String limitParam) {
+        log.debug("TCM01 topicName={}, partition={}, offset={}, sort={}, order={}, offsetParam={}, limit={}", topicName, partitions, offset, sort, order, offsetShiftParam, limitParam);
+        int limit = Integer.parseInt(limitParam);
+        long offsetShift = Long.parseLong(offsetShiftParam);
+        Properties props = createCommonProperties();
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            List<PartitionInfo> partitionInfos = consumer.partitionsFor(topicName);
+            log.debug("TCM02 partitionInfos.size={}, partitionInfos={}", partitionInfos.size(), partitionInfos);
+            List<TopicPartition> topicPartitions = new ArrayList<>();
+            for (int i = 0; i < partitionInfos.size(); i++) {
+                topicPartitions.add(new TopicPartition(topicName, i));
+            }
+            consumer.assign(topicPartitions);
 
-			int[] partitionsArray;
-			if(partitions.equalsIgnoreCase("all")){
-				partitionsArray = IntStream.rangeClosed(0, topicPartitions.size() - 1).toArray();
-			}else {
-				partitionsArray =  Arrays.stream(partitions.split(",")).mapToInt(Integer::parseInt).toArray();
-			}
+            int[] partitionsArray;
+            if (partitions.equalsIgnoreCase("all")) {
+                partitionsArray = IntStream.rangeClosed(0, topicPartitions.size() - 1).toArray();
+            } else {
+                partitionsArray = Arrays.stream(partitions.split(",")).mapToInt(Integer::parseInt).toArray();
+            }
 
-			Map<Integer, Long> beginningOffsets = consumer
-					.beginningOffsets(topicPartitions).entrySet().stream()
-					.collect(Collectors.toMap(k -> k.getKey().partition(), Map.Entry::getValue));
-			Map<Integer, Long> endOffsets = consumer.endOffsets(topicPartitions).entrySet()
-					.stream().collect(Collectors.toMap(k -> k.getKey().partition(), Map.Entry::getValue));
-			log.debug("TCM03 beginningOffsets={}", beginningOffsets);
-			log.debug("TCM04 endOffsets={}", endOffsets);
+            Map<Integer, Long> beginningOffsets = consumer
+                    .beginningOffsets(topicPartitions).entrySet().stream()
+                    .collect(Collectors.toMap(k -> k.getKey().partition(), Map.Entry::getValue));
+            Map<Integer, Long> endOffsets = consumer.endOffsets(topicPartitions).entrySet()
+                    .stream().collect(Collectors.toMap(k -> k.getKey().partition(), Map.Entry::getValue));
+            log.debug("TCM03 beginningOffsets={}", beginningOffsets);
+            log.debug("TCM04 endOffsets={}", endOffsets);
 
-			for (int j : partitionsArray) {
+            for (int j : partitionsArray) {
 
-				Long beginningOffsetForPartition = beginningOffsets.get(j);
-				log.debug("TCM05 beginningOffsetForPartition={}", beginningOffsetForPartition);
+                Long beginningOffsetForPartition = beginningOffsets.get(j);
+                log.debug("TCM05 beginningOffsetForPartition={}", beginningOffsetForPartition);
+                long position = consumer.position(topicPartitions.get(j)) - offsetShift;
+                log.debug("TCM06 position={}", position);
+                long seekTo = position - (limit / partitionsArray.length);
+                if (seekTo > beginningOffsetForPartition) {
+                    log.debug("TCM11 seekTo={}", seekTo);
+                    consumer.seek(topicPartitions.get(j), seekTo);
+                } else {
+                    log.debug("TCM12 seekToBeginning");
+                    consumer.seekToBeginning(Collections.singletonList(topicPartitions.get(j)));
+                }
+            }
 
-				long position;
+            List<Message> messages = new ArrayList<>();
+            int i = 0;
+            // couple first polls after seek don't return eny records
+            while (i < 100 && messages.size() < limit) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10));
+                for (ConsumerRecord<String, String> record : records) {
+                    if (messages.size() < limit) {
+                        messages.add(Message
+                                .builder()
+                                .key(record.key())
+                                .value(record.value())
+                                .offset(record.offset())
+                                .partition(record.partition())
+                                .timestamp(record.timestamp())
+                                .build());
+                    }
+                }
+                i++;
+            }
+            log.debug("TCM20 poll completed records.size={}", messages.size());
+            messages.sort(Comparator.comparing(Message::getTimestamp));
+            TopicMessages topicMessages = TopicMessages.builder()
+                    .messages(messages)
+                    .partitionOffsets(beginningOffsets)
+                    .partitionEndOffsets(endOffsets)
+                    // pagination works only for single selected partition
+                    .totalResults(partitionsArray.length == 1 ? endOffsets.get(partitionsArray[0]) : null)
+                    .build();
+            log.debug("TCM99 topicName={}, partition={}, offset={} topicMessages.size={}", topicName, partitions, offset, topicMessages.getMessages().size());
+            return topicMessages;
 
-				if ("latest".equals(offset)) {
-					position = consumer.position(topicPartitions.get(j));
-				} else {
-					position = Long.parseLong(offset);
-				}
-				log.debug("TCM06 position={}", position);
-				long seekTo = position - (100/partitionsArray.length);
-				if (seekTo > beginningOffsetForPartition) {
-					log.debug("TCM11 seekTo={}", seekTo);
-					consumer.seek(topicPartitions.get(j), seekTo);
-				} else {
-					log.debug("TCM12 seekToBeginning");
-					consumer.seekToBeginning(Collections.singletonList(topicPartitions.get(j)));
-				}
-			}
-
-			List<Message> messages = new ArrayList<>();
-			int i = 0;
-			// couple first polls after seek don't return eny records
-			while (i < 100 && messages.size() < 100) {
-				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10));
-				if (!records.isEmpty()) {
-					mapRecords(messages, records);
-				}
-				i++;
-			}
-			log.debug("TCM20 poll completed records.size={}", messages.size());
-			messages.sort(Comparator.comparing(Message::getTimestamp));
-			TopicMessages topicMessages = TopicMessages.builder()
-					.messages(messages)
-					.partitionOffsets(beginningOffsets)
-					.partitionEndOffsets(endOffsets)
-					.build();
-			log.debug("TCM99 topicName={}, partition={}, offset={} topicMessages.size={}", topicName, partitions, offset, topicMessages.getMessages().size());
-			return topicMessages;
-
-		}
-	}
+        }
+    }
 
     @PostMapping("/api/topic/send/{topic}/{count}")
     @EntryExitLogger
@@ -117,20 +127,6 @@ public class TopicController {
         }
         kafkaTemplate.flush();
     }
-
-	private void mapRecords(List<Message> messages,
-							ConsumerRecords<String, String> records) {
-		for (ConsumerRecord<String, String> record : records) {
-			messages.add(Message
-					.builder()
-					.key(record.key())
-					.value(record.value())
-					.offset(record.offset())
-					.partition(record.partition())
-					.timestamp(record.timestamp())
-					.build());
-		}
-	}
 
     private String replaceTokens(String data, int i) {
         return data
