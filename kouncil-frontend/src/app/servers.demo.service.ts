3d2a6f9cbd8def5bc94ca7d8f9408222b7e3527d
Jacek Grobelny <jgrobelny@consdata.pl>
2021-06-29 10:12:03 +0200

    refactor servers service

--- kouncil-frontend/src/app/servers.demo.service.ts
+++ kouncil-frontend/src/app/servers.demo.service.ts
@@ -2,28 +2,18 @@ import {Injectable} from '@angular/core';
 import {Server} from './server';
 import {ServersService} from './servers.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ServersDemoService implements ServersService {
-  servers: Server[] = [
-    new Server('1', 'first.server.local:9092'),
-    new Server('2', 'second.server.local:9092')
-  ];
-  selectedServerId = '1';
+@Injectable()
+export class ServersDemoService extends ServersService {
 
   constructor() {
+    super();
   }
 
   load() {
+    this.servers = [
+      new Server('1', 'first.server.local:9092'),
+      new Server('2', 'second.server.local:9092')
+    ];
+    this.selectedServerId = '1';
   }
-
-  getSelectedServerId() {
-    return this.selectedServerId;
-  }
-
-  getServers(): Server[] {
-    return this.servers;
-  }
-
 }
--- kouncil-frontend/src/app/servers.service.ts
+++ kouncil-frontend/src/app/servers.service.ts
@@ -1,38 +1,20 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
-import {Backend} from './app.backend';
-import {ServersBackendService} from './servers.backend.service';
-import {ServersDemoService} from './servers.demo.service';
 import {Server} from './server';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ServersService {
+@Injectable()
+export abstract class ServersService {
+  servers: Server[] = [];
   selectedServerId: string;
-  constructor() {
+  protected constructor() {
   }
 
-  load() {
-  }
+  abstract load();
 
   getSelectedServerId() {
     return this.selectedServerId;
   }
 
   getServers(): Server[] {
-    return null;
-  }
-}
-
-export function serverServiceFactory(http: HttpClient): ServersService {
-  switch (environment.backend) {
-    case Backend.SERVER: {
-      return new ServersBackendService(http);
-    }
-    case Backend.DEMO: {
-      return new ServersDemoService();
-    }
+    return this.servers;
   }
 }
