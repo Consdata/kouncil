9b85ad477c4fb9fce8adc20f6eaeb444c744c812
Jacek Grobelny <jacek.grobelny@gmail.com>
2021-08-18 10:44:09 +0200

    IKC-85 Handle empty topics (#173)

--- kouncil-backend/src/main/java/com/consdata/kouncil/track/TopicMetadata.java
+++ kouncil-backend/src/main/java/com/consdata/kouncil/track/TopicMetadata.java
@@ -8,10 +8,23 @@ import java.util.Map;
 
 @Data
 @Builder
-public class TrackTopicMetadata {
+@SuppressWarnings("java:S6212") //val
+public class TopicMetadata {
     private String topicName;
     private Map<Integer, TopicPartition> partitions; // (index, metadata)
     private Map<Integer, Long> beginningOffsets; // (index, beginning offset)
     private Map<Integer, Long> endOffsets; // (index, end offset)
     private Long trackSize;
+
+    public Long getPartitionRangeSize(Integer partition) {
+        return endOffsets.get(partition) - beginningOffsets.get(partition);
+    }
+
+    public Long getAllPartitionRangeSize() {
+        long size = 0L;
+        for (Integer key : partitions.keySet()) {
+            size += getPartitionRangeSize(key);
+        }
+        return size;
+    }
 }
