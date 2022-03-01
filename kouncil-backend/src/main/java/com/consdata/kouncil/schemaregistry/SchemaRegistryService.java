d1c22119af8c4383c478c415d54acbd3ad82d50f
mkociszewski <mkociszewski@consdata.pl>
2022-03-01 18:01:43 +0100

    IKC-177 schema registry service

--- kouncil-backend/src/main/java/com/consdata/kouncil/schemaregistry/SchemaRegistryService.java
+++ kouncil-backend/src/main/java/com/consdata/kouncil/schemaregistry/SchemaRegistryService.java
@@ -10,14 +10,11 @@ import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import io.confluent.kafka.schemaregistry.json.JsonSchemaProvider;
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider;
-import org.apache.kafka.common.utils.Bytes;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class SchemaRegistryService {
     private static final int SCHEMA_CACHE_SIZE = 100;
@@ -33,24 +30,15 @@ public class SchemaRegistryService {
     /**
      * This method is performance-safe, because uses Schema cache
      */
-    public ParsedSchema getKeySchemaById(String topic, int id) throws RestClientException, IOException {
+    public ParsedSchema getKeySchemaBySubjectAndId(String topic, int id) throws RestClientException, IOException {
         return schemaRegistryClient.getSchemaBySubjectAndId(topic.concat(KEY_SCHEMA_SUFFIX), id);
     }
 
     /**
      * This method is performance-safe, because uses Schema cache
      */
-    public ParsedSchema getValueSchemaById(String topic, int id) throws RestClientException, IOException {
+    public ParsedSchema getValueSchemaBySubjectAndId(String topic, int id) throws RestClientException, IOException {
         return schemaRegistryClient.getSchemaBySubjectAndId(topic.concat(VALUE_SCHEMA_SUFFIX), id);
-    }
-
-    /**
-     * Schema identifier is fetched from message, because schema could have changed.
-     * Latest schema may be too new for this record.
-     */
-    public Optional<Integer> getSchemaId(Bytes message) {
-        ByteBuffer buffer = ByteBuffer.wrap(message.get());
-        return buffer.get() == 0 ? Optional.of(buffer.getInt()) : Optional.empty();
     }
 
     private SchemaRegistryClient createSchemaRegistryClient(SchemaRegistryConfig schemaRegistryConfig) {
