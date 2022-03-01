635040486d6dda1633aac9be0d8f7e303081ecd3
mkociszewski <mkociszewski@consdata.pl>
2022-03-01 13:59:50 +0100

    IKC-179 Protobuf consumption

--- kouncil-backend/src/main/java/com/consdata/kouncil/serde/deserialization/DeserializationService.java
+++ kouncil-backend/src/main/java/com/consdata/kouncil/serde/deserialization/DeserializationService.java
@@ -1,12 +1,37 @@
 package com.consdata.kouncil.serde.deserialization;
 
+import com.consdata.kouncil.serde.MessageFormat;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.utils.Bytes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeserializationService {
+
+    private final SchemaRegistryClient schemaRegistryClient;
+
+    public DeserializationService(SchemaRegistryClient schemaRegistryClient) {
+        this.schemaRegistryClient = schemaRegistryClient;
+    }
+
     public DeserializedValue deserialize(ConsumerRecord<Bytes, Bytes> message) {
+        MessageFormat keyFormat = getSchemaForKey(message.topic());
+
+        // new MessageDeserializer(schemaRegistryClient) etc.
+
+        MessageFormat valueFormat = getSchemaForValue(message.topic());
+
         return null;
+    }
+
+    private MessageFormat getSchemaForKey(String topic) {
+        // TODO pobranie ze schema registry
+        return MessageFormat.PROTOBUF;
+    }
+
+    private MessageFormat getSchemaForValue(String topic) {
+        // TODO pobranie ze schema registry
+        return MessageFormat.PROTOBUF;
     }
 }
