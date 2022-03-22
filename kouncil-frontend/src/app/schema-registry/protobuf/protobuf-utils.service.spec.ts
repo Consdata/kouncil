e14646ecef537457af8afcda08fd938da2e5a22b
Paweł Korobczyński <pkorobczynski@consdata.com>
2022-03-22 14:12:26 +0100

    IKC-188 service filling protobuf schema with random values

--- kouncil-frontend/src/app/schema-registry/protobuf/protobuf-utils.service.spec.ts
+++ kouncil-frontend/src/app/schema-registry/protobuf/protobuf-utils.service.spec.ts
@@ -1,30 +1,31 @@
 import { ProtobufUtilsService } from './protobuf-utils.service';
 import expectedProtobufWithData from './expectedProtobufWithData.json';
 
-const testProtobufSchema = 'syntax = "proto3";' +
-  'package test;' +
-  'message Test {' +
-  '   string stringTest = 1;' +
-  '   repeated int32 intTest = 2;' +
-  '   bool booleanTest = 3;' +
-  '   bytes bytesTest = 4;' +
-  '   double doubleTest = 5;' +
-  '   Test1 test1 = 6;' +
-  '}' +
-  'message Test1 {' +
-  '   string stringTest1 = 1;' +
-  '   repeated Test2 test2 = 2;' +
-  '}' +
-  'message Test2 {' +
-  '   double doubleTest2 = 1;' +
-  '}';
+const testProtobufSchema = `
+  syntax = "proto3";
+  package test;
+
+  message Test {
+    string stringTest = 1;
+    repeated int32 intTest = 2;
+    bool booleanTest = 3;
+    bytes bytesTest = 4;
+    double doubleTest = 5;
+    Test1 test1 = 6;
+  }
+
+  message Test1 {
+    string stringTest1 = 1;
+    repeated Test2 test2 = 2;
+  }
+
+  message Test2 {
+    double doubleTest2 = 1;
+  }
+`;
 
 describe('ProtobufUtilsService', () => {
-  let service: ProtobufUtilsService;
-
-  beforeEach(() => {
-    service = new ProtobufUtilsService();
-  });
+  const service: ProtobufUtilsService = new ProtobufUtilsService();
 
   it('should fill protobuf schema with proper data', () => {
     spyOn<any>(service, 'getRandomInt').and.returnValue(123);
