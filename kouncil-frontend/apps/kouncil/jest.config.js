dca63a8f90aefa6ee7798635138a4967547e56a0
dmejer <dmejer@consdata.com>
2022-03-24 11:14:36 +0100

    IKC-191 nx rename kouncil-frontend to kouncil

--- kouncil-frontend/apps/kouncil/jest.config.js
+++ kouncil-frontend/apps/kouncil/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'kouncil-frontend',
+  displayName: 'kouncil',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/kouncil-frontend',
+  coverageDirectory: '../../coverage/apps/kouncil',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
