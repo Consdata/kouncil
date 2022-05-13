54a0c7fb099edb00cbd01d65dc1426e13373a6d2
dmejer <dmejer@consdata.com>
2022-05-13 15:55:56 +0200

    IKC-202 message viewer component onpush

--- kouncil-frontend/apps/kouncil/src/app/topic/message/message-view.component.ts
+++ kouncil-frontend/apps/kouncil/src/app/topic/message/message-view.component.ts
@@ -1,67 +1,80 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {SendComponent} from '../../send/send.component';
 import {DrawerService} from '../../util/drawer.service';
-import {first} from 'rxjs/operators';
+import {first, map} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {TrackService} from '../../track/track.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {Clipboard} from '@angular/cdk/clipboard';
 import {Model} from '@swimlane/ngx-datatable';
 import {MessageData, MessageDataService} from '@app/message-data';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-message-view',
   template: `
-    <mat-dialog-content *ngIf="messageData$ | async as messageData">
+    <mat-dialog-content *ngIf="vm$ | async as vm">
       <div class="drawer-header">
         <div class="drawer-title">Event preview</div>
         <div class="spacer"></div>
         <mat-icon mat-dialog-close class="close">close</mat-icon>
       </div>
-      <div class="headers" *ngIf="messageData.headers.length > 0 && isAnimationDone">
+      <div class="headers" *ngIf="vm.messageData.headers.length > 0 && vm.isAnimationDone">
         <div class="label">Headers</div>
         <ngx-datatable class="headers-table-detail material"
-                       [rows]="messageData.headers"
+                       [rows]="vm.messageData.headers"
                        [rowHeight]="38"
                        [headerHeight]="38"
                        [scrollbarH]="false"
                        [scrollbarV]="false"
                        [columnMode]="'force'"
-                       (activate)="navigateToTrack($event, messageData)">
+                       (activate)="navigateToTrack($event, vm.messageData)">
           <ngx-datatable-column prop="key" name="header key"></ngx-datatable-column>
           <ngx-datatable-column prop="value" name="header value"></ngx-datatable-column>
         </ngx-datatable>
-        <div *ngIf="!isAnimationDone" class="kafka-progress"></div>
+        <div *ngIf="!vm.isAnimationDone" class="kafka-progress"></div>
       </div>
       <div class="payload">
         <div class="key-section">
-          <div class="label">Key (deserialized from {{ messageData.keyFormat }} format)</div>
-          <ngx-json-viewer class="message-payload" [json]="messageData.key"></ngx-json-viewer>
+          <div class="label">Key (deserialized from {{ vm.messageData.keyFormat }} format)</div>
+          <ngx-json-viewer class="message-payload" [json]="vm.messageData.key"></ngx-json-viewer>
         </div>
         <div class="value-section">
-          <div class="label">Value (deserialized from {{ messageData.valueFormat }} format)</div>
-          <ngx-json-viewer class="message-payload" [json]="messageData.value"></ngx-json-viewer>
+          <div class="label">Value (deserialized from {{ vm.messageData.valueFormat }} format)</div>
+          <ngx-json-viewer class="message-payload" [json]="vm.messageData.value"></ngx-json-viewer>
         </div>
       </div>
 
       <div class="actions">
         <button type="button" mat-dialog-close mat-button disableRipple class="cancel">Cancel</button>
         <span class="spacer"></span>
-        <button mat-button disableRipple class="cancel" (click)="copyToClipboard(messageData.value)">Copy to clipboard
+        <button mat-button disableRipple class="cancel" (click)="copyToClipboard(vm.messageData.value)">Copy to
+          clipboard
         </button>
-        <button mat-button disableRipple class="action" (click)="resend(messageData)">Resend event</button>
+        <button mat-button disableRipple class="action" (click)="resend(vm.messageData)">Resend event</button>
       </div>
 
     </mat-dialog-content>
   `,
-  styleUrls: ['./message-view.component.scss']
+  styleUrls: ['./message-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessageViewComponent implements OnInit {
-  messageData$: Observable<MessageData> = this.messageDataService.messageData$;
 
-  public isAnimationDone: boolean = false;
+  private isAnimationDone$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  vm$: Observable<{
+    messageData: MessageData, isAnimationDone: boolean
+  }> = combineLatest([
+    this.messageDataService.messageData$,
+    this.isAnimationDone$
+  ]).pipe(
+    map(([messageData, isAnimationDone]) => (
+        {messageData, isAnimationDone}
+      )
+    )
+  );
 
   constructor(
     private drawerService: DrawerService,
@@ -91,7 +104,7 @@ export class MessageViewComponent implements OnInit {
     // ngx datatable gets its width completely wrong
     // if displayed before container reaches its final size
     this.dialogRef.afterOpened().pipe(first()).subscribe(() => {
-      this.isAnimationDone = true;
+      this.isAnimationDone$.next(true);
     });
   }
 
