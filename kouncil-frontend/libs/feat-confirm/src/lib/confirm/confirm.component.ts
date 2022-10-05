e2cee56dc67f2b4580ebdf24a3c3ecc8466f5c84
dmejer <dmejer@consdata.com>
2022-10-05 10:50:42 +0200

    SHARKS-7597 feat resend confirmation dialog

--- kouncil-frontend/libs/feat-confirm/src/lib/confirm/confirm.component.ts
+++ kouncil-frontend/libs/feat-confirm/src/lib/confirm/confirm.component.ts
@@ -1,39 +1,36 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {ConfirmModel} from './confirm.model';
 
 @Component({
   selector: 'app-confirm',
   template: `
-    <div class="header">
+    <div mat-dialog-title class="header">
       <div class="title">{{data.title}}</div>
       <div class="spacer"></div>
       <mat-icon mat-dialog-close class="close">close</mat-icon>
     </div>
 
-    <div class="subtitle">{{data.subtitle}}</div>
+    <mat-dialog-content class="mat-typography">
+      <div class="subtitle">{{data.subtitle}}</div>
 
-    <div class="section">
-      <div>{{data.sectionLine1}}</div>
-      <div *ngIf="data.sectionLine2">{{data.sectionLine2}}</div>
-    </div>
+      <div class="section">
+        <div>{{data.sectionLine1}}</div>
+        <div *ngIf="data.sectionLine2">{{data.sectionLine2}}</div>
+      </div>
+    </mat-dialog-content>
 
-    <div class="actions">
-      <button type="button" mat-dialog-close mat-button disableRipple class="action-button-white">No</button>
-      <button mat-button disableRipple class="action-button-black" (click)="confirm()">Yes</button>
-    </div>
+    <mat-dialog-actions class="actions">
+      <button mat-button mat-dialog-close disableRipple class="action-button-white">No</button>
+      <button mat-button disableRipple [mat-dialog-close]="true" class="action-button-black">Yes</button>
+    </mat-dialog-actions>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./confirm.component.scss']
 })
 export class ConfirmComponent {
 
-  constructor(
-    private dialogRef: MatDialogRef<ConfirmComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ConfirmModel) {
-  }
-
-  confirm(): void {
-    this.dialogRef.close(true);
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ConfirmModel) {
   }
 
 }
