549b58829b9b6c665cb0d3c0fc0f98b4e950ee29
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2022-12-30 11:43:58 +0100

    IKC-224 Empty topic message list

---
+++ kouncil-backend/src/test/java/com/consdata/kouncil/topic/TopicServiceTest.java
@@ -0,0 +1,47 @@
+package com.consdata.kouncil.topic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.consdata.kouncil.KafkaConnectionService;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 4, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+class TopicServiceTest {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private TopicService topicService;
+
+    @Autowired
+    protected KafkaConnectionService kafkaConnectionService;
+
+    private static final String BOOSTRAP_SERVER = "localhost_9092";
+
+    @Test
+    void should_fetch_all_generated_messages() {
+        IntStream.range(0, 100).forEach(index -> kafkaTemplate.send("embedded-test-topic", String.format("Msg no %s", index)));
+        kafkaConnectionService.getAdminClient(BOOSTRAP_SERVER);
+
+        TopicMessagesDto topicMessages = topicService.getTopicMessages("embedded-test-topic", "all", "1", "100", null, null, null, BOOSTRAP_SERVER);
+        assertThat(topicMessages.getMessages()).hasSize(100);
+    }
+
+    @Test
+    void should_fetch_all_generated_messages_small_amount() {
+        IntStream.range(0, 2).forEach(index -> kafkaTemplate.send("embedded-test-topic-2", String.format("Msg no %s", index)));
+        kafkaConnectionService.getAdminClient(BOOSTRAP_SERVER);
+
+        TopicMessagesDto topicMessages = topicService.getTopicMessages("embedded-test-topic-2", "all", "1", "10", null, null, null, BOOSTRAP_SERVER);
+        assertThat(topicMessages.getMessages()).hasSize(2);
+    }
+}
