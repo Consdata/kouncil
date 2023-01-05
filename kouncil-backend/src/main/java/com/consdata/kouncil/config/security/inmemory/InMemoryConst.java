6a8f0e063de6696c2f402adbb424c1002b728c21
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2023-01-05 18:15:27 +0100

    IKC-260 UI changes

---
+++ kouncil-backend/src/main/java/com/consdata/kouncil/config/security/inmemory/InMemoryConst.java
@@ -0,0 +1,10 @@
+package com.consdata.kouncil.config.security.inmemory;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class InMemoryConst {
+
+    public static final String ADMIN_CONFIG = "default_admin_password.txt";
+}
