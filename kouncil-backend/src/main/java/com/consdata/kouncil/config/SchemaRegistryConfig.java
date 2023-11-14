fa438f999d6c5ee9ae260abd9c9632964e70bdf4
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2023-11-14 19:55:12 +0100

    IKC-328 Configuration to connect Kouncil to secured Schema Registry

--- kouncil-backend/src/main/java/com/consdata/kouncil/config/SchemaRegistryConfig.java
+++ kouncil-backend/src/main/java/com/consdata/kouncil/config/SchemaRegistryConfig.java
@@ -3,12 +3,51 @@ package com.consdata.kouncil.config;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.core.io.Resource;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SchemaRegistryConfig {
+
     private String url;
+    private SchemaRegistrySecurity security;
+    private SchemaRegistrySSL ssl;
+    private SchemaRegistryAuth auth;
+
+    @Getter
+    @Setter
+    public static class SchemaRegistrySecurity {
+
+        private String protocol;
+    }
+
+    @Getter
+    @Setter
+    public static class SchemaRegistrySSL {
+
+        private String keyPassword;
+        private String keyStoreCertificateChain;
+        private String keyStoreKey;
+        private Resource keyStoreLocation;
+        private String keyStorePassword;
+        private String keyStoreType;
+        private String trustStoreCertificates;
+        private Resource trustStoreLocation;
+        private String trustStorePassword;
+        private String trustStoreType;
+        private String protocol;
+    }
+
+    @Getter
+    @Setter
+    public static class SchemaRegistryAuth {
+
+        private String source;
+        private String userInfo;
+    }
 }
