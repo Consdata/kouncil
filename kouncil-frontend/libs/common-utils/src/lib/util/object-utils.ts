65caf697abf53a6ebba96b281c9f397d8cb11804
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2024-05-07 19:23:24 +0200

    IKC-383 Event preview

--- kouncil-frontend/libs/common-utils/src/lib/util/object-utils.ts
+++ kouncil-frontend/libs/common-utils/src/lib/util/object-utils.ts
@@ -2,13 +2,15 @@ export class ObjectUtils {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
   public static removeNull(value: any): string {
-    Object.keys(value).forEach(key => {
-      if (value[key] && typeof value[key] === 'object') {
-        this.removeNull(value[key]);
-      } else if (value[key] === null) {
-        delete value[key];
-      }
-    });
+    if (value) {
+      Object.keys(value).forEach(key => {
+        if (value[key] && typeof value[key] === 'object') {
+          this.removeNull(value[key]);
+        } else if (value[key] === null) {
+          delete value[key];
+        }
+      });
+    }
     return value;
   }
 }
