54ba76f367e8a4824cbed9b547f8d6097295e7f1
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2024-08-05 09:38:43 +0200

    IKC-395 User groups - list, create, update, delete

--- kouncil-backend/src/main/resources/db/migration/V6__user_groups.sql
+++ kouncil-backend/src/main/resources/db/migration/V6__user_groups.sql
@@ -7,9 +7,28 @@ FROM function;
 
 ALTER TABLE function
     add column function_group VARCHAR(40);
-ALTER TABLE user_group
-    add column code VARCHAR(255) unique;
-alter table user_group drop constraint user_group_name_key;
+
+
+DROP TABLE functions_user_groups;
+DROP TABLE user_group;
+
+create table user_group
+(
+    id   bigint              not null primary key,
+    code varchar(255) unique not null,
+    name varchar(255)        not null
+);
+
+create table functions_user_groups
+(
+    function_id   bigint not null
+        constraint fkm881e9dsmi47fw56l92mgc8gq references function,
+    user_group_id bigint not null
+        constraint fkovb2eut0o2pwk7si60ue5f63i references user_group,
+    primary key (function_id, user_group_id)
+);
+
+
 
 insert into function(id, name, label, function_group)
 VALUES (nextval('SEQ_FUNCTION'), 'TOPIC_LIST', 'Topic list', 'TOPIC'),
@@ -20,9 +39,12 @@ VALUES (nextval('SEQ_FUNCTION'), 'TOPIC_LIST', 'Topic list', 'TOPIC'),
        (nextval('SEQ_FUNCTION'), 'TOPIC_MESSAGES', 'View topic messages', 'TOPIC'),
        (nextval('SEQ_FUNCTION'), 'TOPIC_SEND_MESSAGE', 'Send message', 'TOPIC'),
        (nextval('SEQ_FUNCTION'), 'TOPIC_RESEND_MESSAGE', 'Resend message', 'TOPIC'),
-       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_LIST', 'View consumer group list', 'CONSUMER_GROUP'),
-       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_DELETE', 'Delete consumer group', 'CONSUMER_GROUP'),
-       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_DETAILS', 'View consumer group details', 'CONSUMER_GROUP'),
+       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_LIST', 'View consumer group list',
+        'CONSUMER_GROUP'),
+       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_DELETE', 'Delete consumer group',
+        'CONSUMER_GROUP'),
+       (nextval('SEQ_FUNCTION'), 'CONSUMER_GROUP_DETAILS', 'View consumer group details',
+        'CONSUMER_GROUP'),
        (nextval('SEQ_FUNCTION'), 'TRACK_LIST', 'View event track list', 'TOPIC'),
        (nextval('SEQ_FUNCTION'), 'SCHEMA_LIST', 'View schema list', 'SCHEMA_REGISTRY'),
        (nextval('SEQ_FUNCTION'), 'SCHEMA_DETAILS', 'View schema details', 'SCHEMA_REGISTRY'),
