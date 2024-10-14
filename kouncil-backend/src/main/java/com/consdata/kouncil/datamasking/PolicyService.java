3cd182a2b24b2c6e005c3e84efe351afee0cffba
Piotr Belke <Piotr.Belke@external.bcgdv.com>
2024-10-14 18:04:02 +0200

    IKC-410 Policy create, view, edit

--- kouncil-backend/src/main/java/com/consdata/kouncil/datamasking/PolicyService.java
+++ kouncil-backend/src/main/java/com/consdata/kouncil/datamasking/PolicyService.java
@@ -1,5 +1,8 @@
 package com.consdata.kouncil.datamasking;
 
+import com.consdata.kouncil.datamasking.converter.PolicyConverter;
+import com.consdata.kouncil.datamasking.converter.PolicyDtoConverter;
+import com.consdata.kouncil.datamasking.dto.PolicyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,16 @@ public class PolicyService {
 
     private final PolicyRepository policyRepository;
 
+    public void savePolicy(PolicyDto policyDto) {
+        policyRepository.save(PolicyConverter.convert(policyDto));
+    }
+
     public void deletePolicy(Long id) {
         policyRepository.deleteById(id);
+    }
+
+    public PolicyDto getPolicyById(Long id) {
+        return policyRepository.findById(id).map(PolicyDtoConverter::convertToDto)
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Policy with id=%s not found", id)));
     }
 }
